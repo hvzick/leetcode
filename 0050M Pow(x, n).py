44b65bcbfeacd61537227f0102d5e66078701fe3
solitaeryheart <faik15748@gmail.com>
2026-04-07 23:03:10 +0530

    added 50 medium

--- 0050M Pow(x, n).py
+++ 0050M Pow(x, n).py
@@ -8,13 +8,13 @@ class Solution:
         if n < 0:                       # If n is negative, we take the reciprocal of x and convert n to positive
             x = 1 / x                   # Take the reciprocal of x for negative powers
             binaryForm = - binaryForm   # Convert n to positive for the loop to work correctly
-        answer = 1                  # Initialize answer to 1, which will hold the final result                  
-        while binaryForm > 0:       # Loop until all bits of the exponent have been processed
-            if binaryForm % 2 == 1: # If the least significant bit of binaryForm is 1, we multiply the current answer by x
-                answer *= x         # Square x for the next bit of the exponent
+        answer = 1                      # Initialize answer to 1, which will hold the final result                  
+        while binaryForm > 0:           # Loop until all bits of the exponent have been processed
+            if binaryForm % 2 == 1:     # If the least significant bit of binaryForm is 1, we multiply the current answer by x
+                answer *= x             # Square x for the next bit of the exponent
             x *= x
-            binaryForm //= 2        # Use integer division to avoid float result
-        return answer               # Return the final result after processing all bits of the exponent
+            binaryForm //= 2            # Use integer division to avoid float result
+        return answer                   # Return the final result after processing all bits of the exponent
 
 mySol = Solution()
 x = 2.00000
