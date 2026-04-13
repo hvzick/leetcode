f1fac96456b45c8aa8938b6f6db832da2fb8ac1d
solitaeryheart <faik15748@gmail.com>
2026-04-13 21:52:07 +0530

    used DNF method to do this in one pass

--- 0075M Sort Colors.py
+++ 0075M Sort Colors.py
@@ -10,13 +10,23 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
-        for i in range(0, n-1):
-            for j in range(i+1, n):         # use simple bubble sort to sort the list
-                if nums[i] > nums[j]:
-                    temp = nums[i]
-                    nums[i] = nums[j]
-                    nums[j] = temp
-        return nums
+        left = 0
+        mid = 0
+        right = n - 1
+        while mid <= right:
+            if nums[mid] == 0:
+                temp = nums[mid]
+                nums[mid] = nums[left]
+                nums[left] = temp
+                left += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            elif nums[mid] == 2:
+                temp = nums[mid]
+                nums[mid] = nums[right]
+                nums[right] = temp
+                right -= 1
 
 
 
