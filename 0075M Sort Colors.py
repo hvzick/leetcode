6d957822d584126a17b2c04c060624ed8038d506
solitaeryheart <faik15748@gmail.com>
2026-04-13 21:54:01 +0530

    added comments

--- 0075M Sort Colors.py
+++ 0075M Sort Colors.py
@@ -9,28 +9,28 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        left = 0
-        mid = 0
-        right = n - 1
-        while mid <= right:
-            if nums[mid] == 0:
-                temp = nums[mid]
-                nums[mid] = nums[left]
-                nums[left] = temp
-                left += 1
-                mid += 1
-            elif nums[mid] == 1:
-                mid += 1
-            elif nums[mid] == 2:
-                temp = nums[mid]
-                nums[mid] = nums[right]
-                nums[right] = temp
-                right -= 1
+        n = len(nums)                       # get array size
+        left = 0                            # boundary for next 0 placement
+        mid = 0                             # current index being processed
+        right = n - 1                       # boundary for next 2 placement
+        while mid <= right:                 # process until unknown region is exhausted
+            if nums[mid] == 0:              # move 0 to the left region
+                temp = nums[mid]            # store current value
+                nums[mid] = nums[left]      # bring left-region value to mid
+                nums[left] = temp           # place 0 into left region
+                left += 1                   # expand left (0s) region
+                mid += 1                    # advance since swapped-in value is handled
+            elif nums[mid] == 1:            # 1 is already in correct middle region
+                mid += 1                    # just move forward
+            elif nums[mid] == 2:            # move 2 to the right region
+                temp = nums[mid]            # store current value
+                nums[mid] = nums[right]     # bring right-region value to mid
+                nums[right] = temp          # place 2 into right region
+                right -= 1                  # shrink right (2s) region
 
 
 
 
-mySol = Solution()
-nums = [2,0,2,1,1,0]
-print(mySol.sortColors(nums))
+mySol = Solution()                          # create solution object
+nums = [2,0,2,1,1,0]                        # sample input
+print(mySol.sortColors(nums))               # function returns None (array is modified in-place)
